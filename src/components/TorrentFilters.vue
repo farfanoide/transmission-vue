57f9f4c150e06c333464d0b4f9ed1107c4fcbc2c
Ivan Karl <ivan6258@gmail.com>
2020-04-26 01:16:12 -0300

    [WIP] adds filters component

--- src/components/TorrentFilters.vue
+++ src/components/TorrentFilters.vue
@@ -1,17 +1,50 @@
 <template>
-  <q-page class="flex flex-center">
-    show and toggle torrent filters
-  </q-page>
+  <div>
+    <ul>
+      <li v-for="filter in filters"
+          @click="toggleFilter(filter)"
+          :key="filter"
+          :class="{active: isActive(filter)}">
+        <!-- TODO: add filter.icon -->
+        {{filter.name}}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+
 export default {
   name: 'TorrentFilters',
+  data() {
+    return {
+      // TODO: add search input to filter by name
+      // TODO: move filters to vuex
+      activeFilter: null,
+      filters: [
+        {icon: 'eye', name: 'All'},
+        {icon: 'eye', name: 'Active'},
+        {icon: 'eye', name: 'Downloading'},
+        {icon: 'eye', name: 'Seeding'},
+        {icon: 'eye', name: 'Paused'},
+        {icon: 'eye', name: 'Finished'},
+      ],
+    }
+  },
+  methods: {
+    toggleFilter: function (filter)
+    {
+      this.activeFilter = (this.isActive(filter)) ? null : filter
+    },
+    isActive: function (filter)
+    {
+      return this.activeFilter == filter
+    }
+  },
   computed: {
-    // torrent-session
-    // torrents
+    // TODO: add computed property that recalculates trackers only when new
+    // torrents are added/removed
   }
 }
 </script>
-
