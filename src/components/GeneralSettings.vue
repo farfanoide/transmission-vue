4a5724b1df876b6cf58e46f1262b36af444075a7
chudix <stkboy@gmail.com>
2020-06-06 16:37:54 -0300

    WIP: Encapsulate components in general settings
    Add torrent tab component with it own logic

--- src/components/GeneralSettings.vue
+++ src/components/GeneralSettings.vue
@@ -12,6 +12,7 @@
               vertical
               class="text-teal"
               >
+
               <q-tab name="torrents" icon="cloud_download" label="torrents" />
               <q-tab name="speed" icon="speed" label="speed" />
               <q-tab name="peers" icon="group" label="peers" />
@@ -29,152 +30,133 @@
               transition-next="jump-up"
               >
               <q-tab-panel name="torrents">
-                <div class="text-h5 text-center q-mb-md">Torrents</div>
-                <q-input outlined
-                         v-model="clientSettings['download-dir']"
-                         label="Downloads directory" />
-                <div class="row">
-                  <q-checkbox  v-model="clientSettings['start-added-torrents']"
-                               label="Start when added" />
-                  <q-checkbox  v-model="clientSettings['rename-partial-files']"
-                               label="Append .part to incomplete files" />
-                </div>
-                <div class="text-h5 text-center q-mb-md">Seeding</div>
-                <q-checkbox  v-model="clientSettings['seedRatioLimited']"
-                             label="Stop seeding at ratio" />
-                <q-input outlined
-                         v-model="clientSettings['seedRatioLimit']"
-                         label="seed ratio"
-                         :disable="!clientSettings['seedRatioLimited']"
-                         />
-                <q-checkbox  v-model="clientSettings['idle-seeding-limit-enabled']"
-                             label="stop seeding if idle for" />
-                <q-input outlined
-                         v-model="clientSettings['idle-seeding-limit']"
-                         label="minutes"
-                         :disable="!clientSettings['idle-seeding-limit-enabled']"
-                         />
+                <torrents-setting-tab></torrents-setting-tab>
               </q-tab-panel>
+              <!-- <q-tab-panel name="speed"> -->
 
-              <q-tab-panel name="speed">
-                <div class="text-h5 text-center q-mb-md">Speed Limits</div>
+              <!--   <div class="text-h5 text-center q-mb-md">Speed Limits</div> -->
 
-                <q-checkbox  v-model="clientSettings['speed-limit-up-enabled']"
-                             label="Upload" />
-                <q-input outlined
-                         v-model="clientSettings['speed-limit-up']"
-                         label="kB/s"
-                         :disable="!clientSettings['speed-limit-up-enabled']"
-                         />
-                <q-checkbox  v-model="clientSettings['speed-limit-down-enabled']"
-                             label="Download" />
-                <q-input outlined
-                         v-model="clientSettings['speed-limit-down']"
-                         label="kB/s"
-                         :disable="!clientSettings['speed-limit-down-enabled']"
-                         />
+              <!--   <q-checkbox  v-model="clientSettings['speed-limit-up-enabled']" -->
+              <!--                label="Upload" /> -->
+              <!--   <q-input outlined -->
+              <!--            v-model="clientSettings['speed-limit-up']" -->
+              <!--            label="kB/s" -->
+              <!--            :disable="!clientSettings['speed-limit-up-enabled']" -->
+              <!--            /> -->
+              <!--   <q-checkbox  v-model="clientSettings['speed-limit-down-enabled']" -->
+              <!--                label="Download" /> -->
+              <!--   <q-input outlined -->
+              <!--            v-model="clientSettings['speed-limit-down']" -->
+              <!--            label="kB/s" -->
+              <!--            :disable="!clientSettings['speed-limit-down-enabled']" -->
+              <!--            /> -->
 
-                <div class="text-h5 text-center q-mb-md">Alternative Speed Limits</div>
-                <p class="text-center"> Override normal speed limits manually </p>
-                <q-input outlined
-                         type="number"
-                         v-model="clientSettings['alt-speed-up']"
-                         label="Upload(kB/s)"
-                         />
-                <q-input outlined
-                         type="number"
-                         v-model="clientSettings['alt-speed-down']"
-                         label="Download(kB/s)"
-                         />
-                <q-checkbox  v-model="clientSettings['alt-speed-time-enabled']"
-                             label="Scheduled Times" />
-                <q-select outlined
-                          v-model="clientSettings['alt-speed-time-begin']"
-                          :options="[]"
-                          label="From"
-                          disable
-                          />
-                <q-select outlined
-                          v-model="clientSettings['alt-speed-time-end']"
-                          :options="[]"
-                          label="To"
-                          disable
-                          />
-                <q-select outlined
-                          v-model="clientSettings['alt-speed-time-day']"
-                          :options="[]"
-                          label="On days"
-                          disable
-                          />
+              <!--   <div class="text-h5 text-center q-mb-md">Alternative Speed Limits</div> -->
+              <!--   <p class="text-center"> Override normal speed limits manually </p> -->
+              <!--   <q-input outlined -->
+              <!--            class="q-mb-sm" -->
+              <!--            type="number" -->
+              <!--            v-model="clientSettings['alt-speed-up']" -->
+              <!--            label="Upload(kB/s)" -->
+              <!--            /> -->
+              <!--   <q-input outlined -->
+              <!--            class="q-mb-sm" -->
+              <!--            type="number" -->
+              <!--            v-model="clientSettings['alt-speed-down']" -->
+              <!--            label="Download(kB/s)" -->
+              <!--            /> -->
+              <!--   <q-checkbox  v-model="clientSettings['alt-speed-time-enabled']" -->
+              <!--                label="Scheduled Times" /> -->
+              <!--   <q-sele3ct outlined -->
+              <!--             class="q-mb-sm" -->
+              <!--             v-model="clientSettings['alt-speed-time-begin']" -->
+              <!--             :options="[]" -->
+              <!--             label="From" -->
+              <!--             disable -->
+              <!--             /> -->
+              <!--   <q-select outlined -->
+              <!--             class="q-mb-sm" -->
+              <!--             v-model="clientSettings['alt-speed-time-end']" -->
+              <!--             :options="[]" -->
+              <!--             label="To" -->
+              <!--             disable -->
+              <!--             /> -->
+              <!--   <q-select outlined -->
+              <!--             class="q-mb-sm" -->
+              <!--             v-model="clientSettings['alt-speed-time-day']" -->
+              <!--             :options="[]" -->
+              <!--             label="On days" -->
+              <!--             disable -->
+              <!--             /> -->
 
 
-              </q-tab-panel>
+              <!-- </q-tab-panel> -->
 
-              <q-tab-panel name="peers">
-                <div class="text-h5 text-center q-mb-md">Connections</div>
-                <q-input outlined
-                         type="number"
-                         v-model="clientSettings['peed-limit-per-torrent']"
-                         label="Max peers per torrent"
-                         />
-                <q-input outlined
-                         type="number"
-                         v-model="clientSettings['peed-limit-global']"
-                         label="Max peers overall"
-                         />
-                <div class="text-h5 text-center q-mb-md"> Options </div>
-                <q-select outlined
-                          v-model="clientSettings['encryption']"
-                          :options="options.encryption"
-                          label="Encription mode"
-                          />
+              <!-- <q-tab-panel name="peers"> -->
+              <!--   <div class="text-h5 text-center q-mb-md">Connections</div> -->
+              <!--   <q-input outlined -->
+              <!--            type="number" -->
+              <!--            v-model="clientSettings['peed-limit-per-torrent']" -->
+              <!--            label="Max peers per torrent" -->
+              <!--            /> -->
+              <!--   <q-input outlined -->
+              <!--            type="number" -->
+              <!--            v-model="clientSettings['peed-limit-global']" -->
+              <!--            label="Max peers overall" -->
+              <!--            /> -->
+              <!--   <div class="text-h5 text-center q-mb-md"> Options </div> -->
+              <!--   <q-select outlined -->
+              <!--             v-model="clientSettings['encryption']" -->
+              <!--             :options="options.encryption" -->
+              <!--             label="Encription mode" -->
+              <!--             /> -->
 
-                <q-checkbox  v-model="clientSettings['pex-enabled']"
-                             label="Use PEX to find more peers" />
-                <br />
-                <q-checkbox  v-model="clientSettings['dht-enabled']"
-                             label="Use DHT to find more peers" />
-                <br />
-                <q-checkbox  v-model="clientSettings['lpd-enabled']"
-                             label="Use LDP to find more peers" />
+              <!--   <q-checkbox  v-model="clientSettings['pex-enabled']" -->
+              <!--                label="Use PEX to find more peers" /> -->
+              <!--   <br /> -->
+              <!--   <q-checkbox  v-model="clientSettings['dht-enabled']" -->
+              <!--                label="Use DHT to find more peers" /> -->
+              <!--   <br /> -->
+              <!--   <q-checkbox  v-model="clientSettings['lpd-enabled']" -->
+              <!--               label="Use LDP to find more peers" /> -->
 
-                <div disabled>
-                  <div class="text-h5 text-center q-mb-md"> Blocklist </div>
-                  <q-checkbox  v-model="clientSettings['blocklist-enabled']"
-                               label="Enable blocklist" />
-                  <q-input outlined
-                           bottom-slots
-                           value=""
-                           :disable="!clientSettings['blocklist-enabled']"
-                           readonly
-                           >
-                    <template v-slot:after>
-                      <q-btn dense flat :disable="!clientSettings['blocklist-enabled']">Update</q-btn>
-                    </template>
-                    <template v-slot:hint>Blocklist has {{clientSettings.blocklistSize}} rules</template>
-                  </q-input>
+              <!--   <div disabled> -->
+              <!--     <div class="text-h5 text-center q-mb-md"> Blocklist </div> -->
+              <!--     <q-checkbox  v-model="clientSettings['blocklist-enabled']" -->
+              <!--                  label="Enable blocklist" /> -->
+              <!--     <q-input outlined -->
+              <!--              bottom-slots -->
+              <!--              value="" -->
+              <!--              :disable="!clientSettings['blocklist-enabled']" -->
+              <!--              readonly -->
+              <!--              > -->
+              <!--       <template v-slot:after> -->
+              <!--         <q-btn dense flat :disable="!clientSettings['blocklist-enabled']">Update</q-btn> -->
+              <!--       </template> -->
+              <!--       <template v-slot:hint>Blocklist has {{clientSettings.blocklistSize}} rules</template> -->
+              <!--     </q-input> -->
 
-                </div>
-              </q-tab-panel>
-              <q-tab-panel name="network">
-                <div class="text-h5 text-center q-mb-md">Listening Port</div>
-                <q-input outlined
-                         bottom-slots=""
-                         v-model="clientSettings['peer-port']"
-                         label="Peer listening port"
-                         >
-                  <template v-slot:hint> <del>Port is <strong> OPEN</strong></del></template>
-                </q-input>
-                <q-checkbox  v-model="clientSettings['peer-port-random-on-start']"
-                             label="Randomize port on launch" />
-                <br />
-                <q-checkbox  v-model="clientSettings['port-forwarding-enabled']"
-                             label="Use port forwarding from my router" />
-                <br />
-                <div class="text-h5 text-center q-mb-md">Options</div>
-                <q-checkbox  v-model="clientSettings['utp-enabled']"
-                             label="Enable uTP peer communication" />
-              </q-tab-panel>
+              <!--   </div> -->
+              <!-- </q-tab-panel> -->
+              <!-- <q-tab-panel name="network"> -->
+              <!--   <div class="text-h5 text-center q-mb-md">Listening Port</div> -->
+              <!--   <q-input outlined -->
+              <!--            bottom-slots="" -->
+              <!--            v-model="clientSettings['peer-port']" -->
+              <!--            label="Peer listening port" -->
+              <!--            > -->
+              <!--     <template v-slot:hint> <del>Port is <strong> OPEN</strong></del></template> -->
+              <!--   </q-input> -->
+              <!--   <q-checkbox  v-model="clientSettings['peer-port-random-on-start']" -->
+              <!--                label="Randomize port on launch" /> -->
+              <!--   <br /> -->
+              <!--   <q-checkbox  v-model="clientSettings['port-forwarding-enabled']" -->
+              <!--                label="Use port forwarding from my router" /> -->
+              <!--   <br /> -->
+              <!--   <div class="text-h5 text-center q-mb-md">Options</div> -->
+              <!--   <q-checkbox  v-model="clientSettings['utp-enabled']" -->
+              <!--                label="Enable uTP peer communication" /> -->
+              <!-- </q-tab-panel> -->
             </q-tab-panels>
           </div>
         </div>
@@ -187,39 +169,41 @@
 import TransmissionService from '../services/transmission_service';
 import { mapState } from 'vuex';
 import ClientSettings from '../models/client-settings';
-
+import TorrentsSettingTab from './TorrentsSettingTab';
 export default {
+    components: {
+        TorrentsSettingTab
+    },
     name: "GeneralSettings",
     data () {
-      return {
-        tab: 'torrents',
-        options: {
-          encryption: ClientSettings.EncriptionOptions
-        },
-        clientSettings: new ClientSettings(),
-        opened: false
-      }
+        return {
+            tab: 'torrents',
+            options: {
+                encryption: ClientSettings.EncriptionOptions
+            },
+            opened: false
+        }
     },
-  beforeCreate() {
-    this.transmissionService = new TransmissionService({store: this.$store});
-    this.transmissionService.getClientSettings()
-      .then(response => this.clientSettings.setSettings(this.settings))
-      .catch(error => alert(error));
-  },
     methods: {
-      sendSettingsToServer() {
-        /**
-         * Send settings to the server
-         *
-         */
-        this.transmissionService
-          .setClientSettings(this.clientSettings)
-      }
+        sendSettingsToServer() {
+            /**
+             * Send settings to the server
+             *
+             */
+            // this.transmissionService
+            //     .setClientSettings(this.clientSettings)
+        }
     },
     computed: {
-      ...mapState('configs', ['currentServer']),
-      ...mapState('session', ['settings'])
+        ...mapState('session', ['settings']),
+
     }
 
 }
 </script>
+<style>
+.panel {
+  padding: 0 1em;
+  margin-bottom: 1em;
+}
+</style>
